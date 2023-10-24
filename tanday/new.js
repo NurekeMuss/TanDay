a5663195ef4877f3e59714b4b625070f6c6668f8
NurekeMuss <146543627+NurekeMuss@users.noreply.github.com>
2023-10-25 02:19:24 +0600

    The site helps to choose a university for applicants

---
+++ tanday/new.js
@@ -0,0 +1,23 @@
+	/* console.log(Math.round(3.4) * 8);
+	console.log(Math.random()); 
+	console.log(Math.floor(4.7));
+	console.log(Math.ceil(4.1));
+	console.log(Math.min(45, 89));
+	console.log(Math.max(809, 120)); */
+	/* alert('5' * '3s');
+	alert('6' + '9');
+	alert('2' + (5 + 9));
+	alert(2 + 3 + '1'); */
+	/* document.write('3' * '2');
+	document.write('8' - '2');
+	document.write('43' / '4')
+	document.write('7' % '2');
+	document.write('5s' * '4'); */
+	 getElementById(“yourId”).innerHTML=(-'62') + (-'89');//-151 = -62 + (-89)
+	getElementById(“yourId”).innerHTML= '7' * 3 + '3' * 6;//39 = 7 * 3 + 3 * 6
+	getElementById(“yourId”).innerHTML= '7' * ‘3’ + '3' * ‘6’;
+	alert('abbr' * 3);
+	alert(NaN + 3);
+	alert(8 / 0);
+	alert(-9 / 0);
+ 
---
+++ tanday/scritp.js
@@ -0,0 +1,48 @@
+/* Task - 4 
+console.log(32+67)
+console.log(5654-321)
+console.log(23*45)
+console.log(456/8)
+console.log(345>3)
+console.log("8" === 8) */
+
+
+/* Task - 2 
+console.log("Hello world")*/
+
+/* Task - 3  
+console.log("Hello World Nurik".length); */ 
+
+
+/* 6 */console.log("This is exercise".length * Math.floor(6.7 + 4))
+/* 7 */console.log("894209174".length / Math.floor(5.91 ))
+/* 8 */
+/* 9 */
+/* 10 */
+const r = 5;
+const s = Math.PI * Math.pow(r,2);
+console.log('r is ' + r  + 's is' + s )
+/* 11 */
+const a = 4; 
+const b = 6; 
+
+const so = a * b;
+
+console.log(`The area of the rectangle with sides is ` + a + ' ' + b+ ' '  + so + ' ' );
+
+/* 12 */
+const tc = 25;
+
+const tf = (tc * 9/5) + 32;
+
+console.log(tc + " " + "degrees Celsius is equal to" + tf + " degrees Fahrenheit" );
+
+/* 13 */
+const tf2 = 77;
+
+const tc2 = (tf - 32) * 5/9;
+
+console.log(tf2 + " " + "degrees Fahrenheit is equal to" + tc2 + " degrees Celsius" );
+
+
+/* 14 */
